e39e05a83b504a859a7933aa690ce7d839be7f48
Victor Viale <victor.viale@mfglabs.com>
2018-02-18 18:52:29 +0100

    Ex 1-6

--- src/td01.js
+++ src/td01.js
@@ -1,4 +1,95 @@
-// test
-export const a = 2
-const b = [1, 2]
-export const c = [...b, a]
+// const, let
+
+/**
+ * Exercice 1
+ * Convertis le code ci-dessous en ES6, qu'importe la méthode
+*/
+
+var sum = 0
+var numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+for (var i = 0; i < numbers.length; ++i) {
+  sum += numbers[i]
+}
+
+export var ex1 = sum
+
+/**
+ * Exercice 2
+ * Utilise le spread operator pour concaténener ces deux tableaux.
+ * En même temps, convertis ce code en ES6
+ */
+
+var animals1 = ['dog', 'cat', 'faverjon', 'bird']
+var animals2 = ['lion', 'squirrel', 'bear', 'pig']
+
+export var allTheAnimals = [] // TODO
+export var ex2 = allTheAnimals
+
+/**
+ * Exercice 3
+ * Convertis ce code en ES6 en utilisant les `fat arrow functions`
+ */
+
+function makeDogACat (thing) {
+  if (thing === 'dog') {
+    return 'cat'
+  }
+
+  return thing
+}
+
+export var ex3 = makeDogACat
+
+/**
+ * Exercice 4
+ * Que retourne cette fonction ?
+ */
+
+export function scope () {
+  const a = 'dog'
+  if (a === 'dog') {
+    const a = 'cat'
+    return a
+  } else {
+    return a
+  }
+}
+
+/**
+ * Exercice 5
+ * First-class functions, callbacks
+ *
+ * Crée une fonction qui appelle une fonction (transformNumber)
+ * qui prendra number en premier paramètre pour l'incrémenter.
+ */
+
+const number = 41
+export const transformNumber = () => {} // TODO
+
+export const makeSomethingOutOfNumber = null // TODO
+
+/**
+ * Exercice 6
+ * G coordonne la rédaction de son magazine annuel, et souhaite que chacun de ses rédacteurs puissent choisir entre
+ * quatre signatures possibles, chacune incluant le nom et prénom du rédacteur concerné (au format "<Prénom> <Nom>").
+ * Ecrivez une fonction qui prendra comme arguments le texte de l'article, une de ces quatre signatures
+ * et le nom du rédacteur afin de générer l'article signé.
+ */
+
+export const articleBody = `Le pari de ce projet est de se centrer sur les images du quotidien de l’école et de s’intéresser à la construction
+de l’individu à la lumière des transformations numériques dans l’éducation. Il s’agit de travailler l’espace du hall
+d’accueil du nouveau rectorat pour mettre en résonance l’expérience de ses usagers avec une forme visuelle apprenante,
+générative, et qui réfléchit les expériences d’apprentissage d’aujourd’hui.
+Doué de facultés d’apprentissage automatique, le programme imaginé avec des chercheurs en analyse de l'image - régit,
+répartit et génère les signes graphiques issus d'un index constitué d'observations sur le motif de l'école d'aujourd'hui.
+Selon des critères liés aux étapes d’acquisition de la connaissance, les dessins sont ainsi décomposés, analysés,
+séquencés en grille, et enfin regénérés pour prendre vie et forme dans le nouveau bâtiment.`
+
+export const sig1 = (fullName) => `${fullName} l'immortel.le`
+export const sig2 = (fullName) => `Pickle ${fullName}`
+export const sig3 = (fullName) => `Jean-Michel ${fullName}`
+export const sig4 = (fullName) => `${fullName}, First of Her Name, the Unburnt, Queen of the Andals and the First Men, Khaleesi of the Great Grass Sea, Breaker of Chains, and Mother of Dragons`
+
+export const generateArticleWithSignature = () => {} // TODO
+export const finalArticle = generateArticleWithSignature(/* ... */) // TODO
--- test/td01.test.js
+++ test/td01.test.js
@@ -1,10 +1,51 @@
-import {a, c} from '../src/td01.js'
+'use strict'
+
+import {
+  ex1,
+  ex2,
+  ex3,
+  scope,
+  transformNumber, makeSomethingOutOfNumber,
+  sig1, sig2, sig3, sig4, generateArticleWithSignature, finalArticle
+} from '../src/td01.js'
 
 import chai from 'chai'
 chai.expect()
 const expect = chai.expect
 
-it('exercice 1', () => {
-  expect(a).to.eq(2)
-  expect(c).to.deep.eq([1, 2, 2])
+const test = it
+
+test('exercice 1', () => {
+  expect(ex1).to.eq(54)
+})
+
+test('exercice 2', () => (
+  expect(ex2).to.deep.eq(['dog', 'cat', 'faverjon', 'bird', 'lion', 'squirrel', 'bear', 'pig'])
+))
+
+test('exercice 3', () => {
+  expect(ex3('dog')).to.eq('cat')
+  expect(ex3('anything')).to.eq('anything')
+})
+
+test('exercice 4', () => {
+  expect(scope()).to.eq('cat')
+})
+
+test('exercice 5', () => {
+  expect(makeSomethingOutOfNumber).to.be.a('function')
+  expect(makeSomethingOutOfNumber.length).to.eq(2)
+  expect(makeSomethingOutOfNumber(transformNumber, 41)).to.eq(42)
+})
+
+test('exercice 6', () => {
+  expect(generateArticleWithSignature).to.be.a('function')
+  expect(generateArticleWithSignature.length).to.eq(3)
+  expect(finalArticle).to.match(/enfin régénérés/)
+
+  const hasSig = /Pickle/.test(finalArticle) ||
+    /Jean-Michel/.test(finalArticle) ||
+    /First of Her Name/.test(finalArticle) ||
+    /l'immortel/.test(finalArticle)
+  expect(hasSig).to.eq(true)
 })
