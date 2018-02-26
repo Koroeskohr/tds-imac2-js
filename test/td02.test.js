af9d8087f1e9b5c603bb4b4b115ad91d72c20e4c
Victor Viale <victor.viale@mfglabs.com>
2018-02-26 18:09:04 +0100

    Add correction for TD2

--- test/td02.test.js
+++ test/td02.test.js
@@ -7,7 +7,7 @@ import {
   p,
   generateMarkupForDog,
   generateMarkupForAllDogs
-} from '../src/td02/td02'
+} from '../src/td02/td02_correction'
 
 import state from '../src/td02/state'
 
@@ -21,67 +21,65 @@ test('exercice 1', () => {
   expect(every(x => x > 0 && x < 5, [1, 2, 3, 4])).to.eq(true)
 })
 
-describe('exercice 2', () => {
-  test('createTag', () => {
-    expect(createTag).to.be.a('function')
-    expect(
-      createTag('div', {class: 'header'}, [])
-    ).to.deep.eq({
-      name: 'div',
-      attributes: {class: 'header'},
-      children: []
-    })
+test('createTag', () => {
+  expect(createTag).to.be.a('function')
+  expect(
+    createTag('div', {class: 'header'}, [])
+  ).to.deep.eq({
+    name: 'div',
+    attributes: {class: 'header'},
+    children: []
   })
+})
 
-  describe('exercice 2.1', () => {
-    test('img', () => {
-      expect(img).to.be.a('function')
-      expect(img('http://example.com/img.jpg')).to.deep.eq({
-        name: 'img',
-        attributes: {src: 'http://example.com/img.jpg'},
-        children: []
-      })
-    })
-    test('figure', () => {
-      expect(figure).to.be.a('function')
-      expect(figure([])).to.deep.eq({
-        name: 'figure',
-        attributes: null,
-        children: []
-      })
-    })
-    test('figcaption', () => {
-      expect(figcaption).to.be.a('function')
-      expect(figcaption([])).to.deep.eq({
-        name: 'figcaption',
-        attributes: null,
-        children: []
-      })
-    })
-    test('p', () => {
-      expect(p).to.be.a('function')
-      expect(p(['value'])).to.deep.eq({
-        name: 'p',
-        attributes: null,
-        children: ['value']
-      })
-    })
+test('exercice 2.1 : img', () => {
+  expect(img).to.be.a('function')
+  expect(img({src: 'http://example.com/img.jpg'})).to.deep.eq({
+    name: 'img',
+    attributes: {src: 'http://example.com/img.jpg'},
+    children: []
   })
+})
+test('exercice 2.1 : figure', () => {
+  expect(figure).to.be.a('function')
+  expect(figure(null, [])).to.deep.eq({
+    name: 'figure',
+    attributes: null,
+    children: []
+  })
+})
 
-  test('exercice 2.2', () => {
-    const dogMarkup = generateMarkupForDog('url', 'doggo', 'pretty puppy')
-    expect(dogMarkup.name).to.eq('figure')
-    expect(dogMarkup.children[0].name).to.eq('img')
-    expect(dogMarkup.children[0].attributes.src).to.eq('url')
-    expect(dogMarkup.children[1].name).to.eq('figcaption')
-    expect(dogMarkup.children[1].children[0].name).to.eq('p')
-    expect(dogMarkup.children[1].children[0].children).to.eq(['doggo'])
-    expect(dogMarkup.children[1].children[1].name).to.eq('p')
-    expect(dogMarkup.children[1].children[1].children).to.eq(['pretty puppy'])
+test('exercice 2.1 : figcaption', () => {
+  expect(figcaption).to.be.a('function')
+  expect(figcaption(null, [])).to.deep.eq({
+    name: 'figcaption',
+    attributes: null,
+    children: []
   })
+})
 
-  test('exercice 2.3', () => {
-    const dogsMarkup = generateMarkupForAllDogs(state.dogs)
-    expect(dogsMarkup.name).to.eq('div')
+test('exercice 2.1 : p', () => {
+  expect(p).to.be.a('function')
+  expect(p(null, ['value'])).to.deep.eq({
+    name: 'p',
+    attributes: null,
+    children: ['value']
   })
+})
+
+test('exercice 2.2', () => {
+  const dogMarkup = generateMarkupForDog('url', 'doggo', 'pretty puppy')
+  expect(dogMarkup.name).to.eq('figure')
+  expect(dogMarkup.children[0].name).to.eq('img')
+  expect(dogMarkup.children[0].attributes.src).to.eq('url')
+  expect(dogMarkup.children[1].name).to.eq('figcaption')
+  expect(dogMarkup.children[1].children[0].name).to.eq('p')
+  expect(dogMarkup.children[1].children[0].children).to.deep.eq(['doggo'])
+  expect(dogMarkup.children[1].children[1].name).to.eq('p')
+  expect(dogMarkup.children[1].children[1].children).to.deep.eq(['pretty puppy'])
+})
+
+test('exercice 2.3', () => {
+  const dogsMarkup = generateMarkupForAllDogs(state.dogs)
+  expect(dogsMarkup.name).to.eq('div')
 })
