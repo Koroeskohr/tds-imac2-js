e4c3972a8ec3902efc9b6b6bed66595c2754a655
Victor Viale <victor.viale@mfglabs.com>
2018-02-26 10:34:03 +0100

    try installing hyper

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// left empty
+import { app } from 'hyperapp';
+import actions from './actions';
+import state from './state';
+import view from './components/Counter';
+
+const {
+  add,
+  sub,
+} = app(
+  state,
+  actions,
+  view,
+  document.body,
+);
