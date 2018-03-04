1ba0adba9d397ee48dd567307e1b388d7c1538d1
Victor Viale <victor.viale@mfglabs.com>
2018-03-04 21:02:40 +0100

    Improve hyper testers

--- test/hyperappTestHelpers.js
+++ test/hyperappTestHelpers.js
@@ -10,6 +10,7 @@ export const hasChildrenWithTag = tag => component =>
   findChildrenByTag(tag)(component).length > 0
 
 export const toStructure = (tag) => {
+  if (tag == null) throw new Error('tag is not defined')
   const name = typeof (tag) === 'string' ? '$' : tag.nodeName
   if (tag.children && tag.children.length > 0) {
     return name + '(' + tag.children.map(toStructure).join(',') + ')'
