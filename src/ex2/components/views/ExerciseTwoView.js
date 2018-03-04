9cda4d5548d92a77272e32ebe11989b1e8b872a2
Victor Viale <victor.viale@mfglabs.com>
2018-03-04 20:17:14 +0100

    Fix ex2

--- src/ex2/components/views/ExerciseTwoView.js
+++ src/ex2/components/views/ExerciseTwoView.js
@@ -1,8 +1,3 @@
 import { h } from 'hyperapp'
 
-export default (state, actions) => // TODO
-  <main>
-    <h1>{state.name}</h1>
-    <h2>{state.job}</h2>
-    <p>Got a life sentence : {state.gotLifeSentence ? 'yes' : 'no'}</p>
-  </main>
+export default (state, actions) => null // TODO
