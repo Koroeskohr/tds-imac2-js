6205a82b802c6ac61eae6e28c2166698c3e493ed
Auram20 <myriamanik97@gmail.com>
2019-03-18 12:46:05 +0100

    mon avancement

--- src/exercise/components/views/CoursesOverview.js
+++ src/exercise/components/views/CoursesOverview.js
@@ -1,3 +1,21 @@
 import { h } from 'hyperapp'
 
-export default () => h('div', {}, 'allez travaille')
+
+
+export default (state, actions) =>
+    h('main',{},
+        [
+            h('header', {}, {}),
+            
+            h('ul', {}, ['LISTE DES PROF', state.teachers.map(teacher =>
+                h('li', {},[
+                    h('img',{src: teacher.photo, style : 'float: left;'},{}),
+                       h('p',{style : 'font-size: 44px;'},teacher.name), h('p',{style : 'font-size: 12px;'},teacher.subject)])
+            ) ]),
+            
+            h('ul', {}, ['LISTE DES MATIERES', state.teachers.map(teacher =>
+                h('li', {}, [teacher.subject])
+            ) ]),
+        ]
+    )
+    
--- src/exercise/state/index.js
+++ src/exercise/state/index.js
@@ -1,2 +1,43 @@
-export default {
+
+export default 
+{
+  teachers:
+  [
+    {
+      name: 'Victor Viale ',
+      subject: 'JS',
+      description: ' Super le js',
+      heures: ' 123 heures ',
+      année: 2019,
+      photo: 'https://avatarfiles.alphacoders.com/321/thumb-32154.gif',
+      categorie: 'science'
+    },
+    {
+      name: 'Emilie Verger ',
+      subject: 'HDA',
+      description: 'vous connaissez ce tableau ? ',
+      heures: '254 heures ',
+      année: 1989,
+      photo: 'https://avatarfiles.alphacoders.com/148/thumb-14825.gif',
+      categorie: 'art'
+    },
+    {
+      name: 'Ozlem Sulak ',
+      subject: 'Language et compréhension du cinéma',
+      description: 'Fellini > Aya nakamura ', 
+      heures: '254 heures ',
+      année: 2019,
+      photo: 'https://avatarfiles.alphacoders.com/548/thumb-54824.jpg',
+      categorie: 'art'
+    },
+    {
+      name: 'Miguel Martinez',
+      subject: 'Mathématiques',
+      heures: '254 heures ',
+      description: 'ça va ou pas', 
+      année: 2019,
+      photo: 'https://avatarfiles.alphacoders.com/577/thumb-57778.png',
+      categorie: 'science'
+    }
+  ]
 }
