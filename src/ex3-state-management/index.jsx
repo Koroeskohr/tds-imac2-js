668e8b066358bd17326a80c774e5a7c78001811f
Victor Viale <victor.viale@besedo.com>
2021-03-29 10:33:26 +0200

    fix linter

--- src/ex3-state-management/index.jsx
+++ src/ex3-state-management/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 // eslint-disable-next-line import/prefer-default-export
-export const StateManagementExercise = () => (
-  <div>
-    <input type="text" />
-
-    <ul>
-      ...
-    </ul>
-  </div>
-);
+export const StateManagementExercise = (props) => {
+  const d = props;
+  return (
+    <div>
+      <input type="text" />
+      {d}
+      <ul>...</ul>
+    </div>
+  );
+};
